1b95811dad7b1624f57eb942e935da3f9518dedd
Weston Beck <wesbeck@msn.com>
2025-07-31 23:01:35 -0700

    Add front end files and some configuration in main

--- grocery-list/main.py
+++ grocery-list/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from controller.item_controller import router as item_router
 from controller.meal_controller import router as meal_router
 from controller.meal_item_controller import router as meal_item_router
@@ -6,9 +10,41 @@ from controller.list_item_controller import router as list_item_router
 
 app = FastAPI()
 
+# Configure CORS to allow frontend JS to call the API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for development; restrict in production
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, DELETE, etc.)
+    allow_headers=["*"]   # Allow all headers
+)
+
+# Mount the static directory to serve static files (JS, CSS, HTML, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+# Serve the main HTML page at the root URL
 @app.get("/")
-def root():
-    return {"message": "Hello"}
+def read_index():
+    return FileResponse("static/index.html")  # Serves index.html on accessing '/'
+
+@app.get("/main")
+async def main_view(request: Request):
+    # fetch or compute the context your JSP expected
+    items = items_db
+    return templates.TemplateResponse("main.html", {"request": request, "items": items})
+
+@app.get("/test")
+async def test_view(request: Request):
+    return templates.TemplateResponse("test.html", {"request": request})
+
+@app.get("/list")
+async def list_view(request: Request):
+    current = current_list
+    return templates.TemplateResponse("list.html", {
+        "request": request,
+        "list_items": current
+    })
 
 app.include_router(item_router)
 app.include_router(meal_router)
---
+++ grocery-list/static/main.js
@@ -0,0 +1,178 @@
+//On load
+var xhr = new XMLHttpRequest();
+var allItems = this.getAllItems();
+var itemEditButtons = [];
+var itemDeleteButtons = [];
+var allMeals = this.getAllMeals();
+var mealEditButtons = [];
+var mealDeleteButtons = [];
+var allListItems = this.getAllListItems();
+
+// Load page
+this.tableLoader(allItems, "mainItemTable");
+
+this.tableLoader(allMeals, "mainMealTable");
+
+this.listLoader();
+
+document.getElementById('addNewItemButton').addEventListener("click", () =>{
+	modalHandler("addItemModal");
+})
+
+document.getElementById('addNewMealButton').addEventListener("click", () =>{
+	addMealModal();
+})
+
+document.getElementById('resetList').addEventListener("click", () =>{
+	this.xhr.open("DELETE", 'http://localhost:8000/list',false);
+	this.xhr.send(null);
+	location.reload();
+})
+
+//Functions
+function getAllItems(){
+	this.xhr.open("GET", 'http://localhost:8000/items',false);
+	this.xhr.send(null);
+	var items = JSON.parse(this.xhr.responseText);
+	
+	return items;
+}
+
+function getAllMeals(){
+	this.xhr.open("GET", 'http://localhost:8000/meals',false);
+	this.xhr.send(null);
+	var meals = JSON.parse(this.xhr.responseText);
+	
+	return meals;
+}
+
+function getAllListItems(){
+	this.xhr.open("GET", 'http://localhost:8000/list',false);
+	this.xhr.send(null);
+	var list = JSON.parse(this.xhr.responseText);
+	
+	return list;
+}
+
+function tableLoader(data,tableId){
+	var table = document.getElementById(tableId);
+	for(var i = 0; i < data.length; i++){
+		var newRow = table.insertRow(i+1)
+		var name = newRow.insertCell(0);
+		var description = newRow.insertCell(1);
+		var addToList = newRow.insertCell(2);
+		var action = newRow.insertCell(3);
+		name.innerHTML = data[i].name;
+		description.innerHTML = data[i].description;
+		if(tableId == "mainItemTable"){
+			addToList.innerHTML = '<button type="button" onclick="addItemToList(this.id.substr(19))" class="addItemToListButton" id="itemAddToListButton'+data[i].id+'">Add To List</button>';
+			action.innerHTML = '<button type="button" onclick="editItemModal(this.id.substr(14))" class="itemEditButton" id="itemEditButton'+data[i].id+
+			'">Edit</button>  <button type="button" onclick="deleteItem(this.id.substr(16))" class="itemDeleteButton" id="itemDeleteButton'+data[i].id+
+			'">Delete</button>';
+		}
+		else if(tableId == "mainMealTable"){
+			addToList.innerHTML = '<button type="button" onclick="addMealItemsToList(this.id.substr(23))" class="addMealItemToListButton" id="mealItemAddToListButton'+data[i].id+'">Add To List</button>';
+			action.innerHTML = '<button type="button" class="mealEditButton" id="mealEditButton'+data[i].id+
+			'">Edit</button> <button type="button" onclick="deleteMeal(this.id.substr(16))" class="mealDeleteButton" id="mealDeleteButton'+data[i].id+'">Delete</button>';
+		}
+	}
+}
+
+function listLoader(){
+	var list = document.getElementById("currentItemsList");
+	for(var i = 0; i < allListItems.length; i++){
+		var newElement = new Option(allListItems[i].name);
+		list.add(newElement);
+	}
+}
+
+function showId(elementId){
+	alert(elementId.substr(16));
+}
+
+function addItemToList(itemId){
+	xhr.open("POST", 'http://localhost:8000/list/items?item_id='+itemId, false);
+	xhr.send(null);
+	location.reload();
+}
+
+function addMealItemsToList(mealId){
+	xhr.open("POST", 'http://localhost:8000/list/?meal_id='+mealId, false);
+	xhr.send(null);
+	location.reload();
+}
+
+function editItemModal(itemId){
+	xhr.open("GET", 'http://localhost:8000/items/'+itemId, false);
+	xhr.send(null);
+	var item = JSON.parse(xhr.responseText);
+	var itemName = document.getElementById("editItemName");
+	itemName.value = item.name;
+	var itemDesc = document.getElementById("editItemDescription");
+	itemDesc.value = item.description;
+	var submit = document.getElementById("editItemSubmit");
+	modalHandler("editItemModal");
+
+	
+	submit.addEventListener("click", () =>{
+		xhr.open("POST", 'http://localhost:8000/items/'+itemId+'?name='+itemName.value+'&description='+itemDesc.value, false);
+		xhr.send(null);
+		location.reload();
+	})
+	
+}
+
+function addMealModal(){
+	xhr.open("GET", 'http://localhost:8000/items', false);
+	xhr.send(null);
+	var availItems = JSON.parse(xhr.responseText);
+	var availItemsList = document.getElementById("addMealAvailItems");
+	var selectItems = [];
+	var selectItemsList = document.getElementById("addMealSelectItems");
+	
+	for(var i = 0; i < availItems.length; i++){
+		var newElement = new Option(availItems[i].name);
+		availItemsList.add(newElement);
+	}
+	
+	modalHandler("addMealModal");
+	
+	//On load: show all available items on the left. Create two temporary arrays: available items and added items
+	//If the user adds an item to the meal, remove from the available items and move to the added items
+	//Once the user submits the meal, save the added items in the meal_item table
+}
+
+function deleteItem(itemId){
+	xhr.open("DELETE", 'http://localhost:8000/items/'+itemId, false);
+	xhr.send(null);
+	location.reload();
+}
+
+function deleteMeal(mealId){
+	xhr.open("DELETE", 'http://localhost:8000/meals/'+mealId+'/items', false);
+	xhr.open("DELETE", 'http://localhost:8000/meals/'+mealId, false);
+	xhr.send(null);
+	location.reload();
+}
+
+function modalHandler(modalId){
+	var modal = document.getElementById(modalId);
+	
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	
+	// When the user clicks the button, open the modal 
+	modal.style.display = "block";
+	
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+	
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+}
